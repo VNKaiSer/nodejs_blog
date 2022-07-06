6dc5a839a10ca13ec3ad71bb8e6f4f1129732bab
unknown <tandatvok16@gmail.com>
2022-07-06 13:41:39 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
+const morgan = require('morgan');
 const path = require('path')
+const hbs = require('express-handlebars');
+
 app.use(morgan('combined'))
 
 // Temple handlebars
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', handlebars());
+
+app.engine('handlebars', hbs.engine());
 app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 
 
+// console.log('PATH: ' + path.join(__dirname, 'resources/views'))
 
-app.get('/trang-chu', (req, res) => {
-    res.send('home')
+app.get('/', (req, res) => {
+    res.render('home')
 })
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
