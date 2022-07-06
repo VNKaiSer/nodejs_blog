7f3b61fcb5896ee4a08e4e95af23358613322bbf
unknown <tandatvok16@gmail.com>
2022-07-06 14:45:02 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,20 +5,27 @@ const morgan = require('morgan');
 const path = require('path')
 const hbs = require('express-handlebars');
 
+
 app.use(morgan('combined'))
 
 // Temple handlebars
 
-app.engine('handlebars', hbs.engine());
-app.set('view engine', 'handlebars');
+app.engine('.hbs', hbs.engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
 
 
 // console.log('PATH: ' + path.join(__dirname, 'resources/views'))
 
+// Trang chủ
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+// TRang tin tức
+app.get('/news', (req, res) => {
+    res.render('news')
 })
 
 app.listen(port, () => {
