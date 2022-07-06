22c46ec916de4f233c656092b34927b989ba6064
unknown <tandatvok16@gmail.com>
2022-07-06 15:05:29 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const path = require('path')
 const hbs = require('express-handlebars');
 
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(morgan('combined'))
 
 // Temple handlebars
